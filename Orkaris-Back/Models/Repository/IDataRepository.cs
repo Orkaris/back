eb5be95edb9e653ae4dc8e7029d9a94be7c3842f
ladretti <ladretti>
2025-04-04 11:49:18 +0200

    Idata repository

---
+++ Orkaris-Back/Models/Repository/IDataRepository.cs
@@ -0,0 +1,12 @@
+using System;
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Orkaris_Back.Models.Repository;
+
+public interface IDataRepository<TEntity>
+{
+    Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+    Task<ActionResult<TEntity>> GetByIdAsync(int id);
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
