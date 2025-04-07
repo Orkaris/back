14401eada859488054e7651d583b6952bde7ff40
ladretti <ladretti>
2025-04-07 10:26:45 +0200

    Migrations 2

---
+++ Orkaris-Back/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using System;
+using Orkaris_Back.Models.DTO;
+using Orkaris_Back.Models.EntityFramework;
+
+namespace Orkaris_Back.Mapping;
+
+public class MappingProfile: AutoMapper.Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, MinimalUserDTO>();
+        CreateMap<User, LoginRequestDTO>();
+        CreateMap<User, RegisterUserDTO>();
+    }
+}
