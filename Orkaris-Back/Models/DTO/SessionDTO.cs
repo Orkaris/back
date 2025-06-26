80bc22d01b59a209c83d0f3f2c63726a618ce036
ladretti <ladrette.irwin@gmail.com>
2025-06-26 14:01:20 +0200

    duration

--- Orkaris-Back/Models/DTO/SessionDTO.cs
+++ Orkaris-Back/Models/DTO/SessionDTO.cs
@@ -25,6 +25,8 @@ public class PostSession2DTO
     public string Name { get; set; } = string.Empty;
     public Guid UserId { get; set; }
     public Guid WorkoutId { get; set; }
+    public TimeSpan Duration { get; set; }
+
     public ICollection<PostExerciseGoalDTO> SessionExerciseSession { get; set; } = new List<PostExerciseGoalDTO>();
 }
 
